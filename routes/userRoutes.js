a0cfcdaaf635d82872467ce9c3ecab6c9222a633
Arik92 <arik92@gmail.com>
2018-05-19 20:24:41 +0300

    update score

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -63,9 +63,9 @@ router.post('/join', function(req, res1, next){
 router.put('/addScore', function(req, res1, next){
   /* UPDATE weather SET temp_lo = temp_lo+1, temp_hi = temp_lo+15, prcp = DEFAULT
   WHERE city = 'San Francisco' AND date = '2003-07-03';*/
-  console.log("adding to ", req);
+  console.log("adding to ", req.body);
   var text = 'UPDATE gfnzpmjz.tictactoe.users SET totalscore = totalscore+1, dailyscore = dailyscore+1, weeklyscore = weeklyscore+1, monthlyscore = monthlyscore+1 WHERE username = $1';  
-  var value = [req.body];
+  var value = [req.body.winnerName];
   pool.query(text, value, (err, res) => {
     if (err) {
       console.log(err.stack);       
